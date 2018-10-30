704ec6d47359aea30a8c0459da7f01c5c0a3ab9d
loa1696 <loa1696@gmail.com>
2018-10-30 17:29:18 +0200

    Initial commit

---
+++ верстка_js_main.js
@@ -0,0 +1,11 @@
+(function ($) {
+    $(window).on("load", function () {
+        var masonryGrid = $(".grid");
+        masonryGrid.imagesLoaded(function () {
+            masonryGrid.masonry({
+                itemSelector: ".grid-item",
+                percentPosition: true
+            });
+        });
+    });
+})(jQuery);
